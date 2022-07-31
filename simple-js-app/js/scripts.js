fe2909519c57a32cf2167eeb6876a6bf2aca58de
brebre383 <108360381+brebre383@users.noreply.github.com>
2022-07-31 10:48:11 -0500

    added pokemon list

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [
+        {       name: 'Ivysaur',
+                height: 1,
+                weight: 13,
+                types: ['grass', 'poison']
+        },
+
+        {       name: 'Blastoise',
+                height: 1.6,
+                weight: 85.5,
+                types: ['water']
+        },
+
+        {       name: 'Girafarig',
+                height: 1.5,
+                weight: 41.5,
+                types: ['psychic', 'normal']
+        },
+];
