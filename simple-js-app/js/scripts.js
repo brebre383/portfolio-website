523a24aa98beed51c1cff6d7747f01f214ebf41c
brebre383 <108360381+brebre383@users.noreply.github.com>
2022-07-31 12:37:10 -0500

    added a loop

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -17,3 +17,17 @@ let pokemonList = [
                 types: ['psychic', 'normal']
         },
 ];
+
+document.write('<div class="pokemonList">');
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write('<div class="pokemon--big">');
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(' - Wow, that\'s big!');
+    } else {
+      document.write('<div class="pokemon">');
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    }
+    document.write('</div>');
+}
+document.write('</div>');
